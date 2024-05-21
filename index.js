e2ee070514bcd4924d4c0d0ffd5104b0a98ab2c8
mehejar <abdalmehejar@gmail.com>
2024-05-21 10:34:12 +0600

    fresh food server started

---
+++ index.js
@@ -0,0 +1,124 @@
+const express = require('express')
+const app = express();
+const cors = require('cors');
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.b9ejgk4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+
+    const productsCollection = client.db('freshFoodDB').collection('products')
+    const cartCollection = client.db('freshFoodDB').collection('cart')
+    const userCollection = client.db('freshFoodDB').collection('user')
+
+    // users Related API
+    app.post('/user', async(req, res) =>{
+      const user = req.body;
+      // insert email if user doesnt exit
+      // you can do this many ways(1.email unq 2. upsert)
+      const query = {email: user.email}
+      const existingUser = await userCollection.findOne(query)
+
+      if(existingUser){
+        return res.send({message: 'user Already exist'})
+      }
+
+      const result = await userCollection.insertOne(user)
+      res.send(result)
+    })
+
+    app.get('/user', async(req, res) =>{
+      const result = await userCollection.find().toArray()
+      res.send(result)
+    })
+
+    // user access
+    app.delete('/user/:id', async (req, res) =>{
+      const id = req.params.id
+      const query = {_id: new ObjectId(id)}
+      const result = await userCollection.deleteOne(query)
+      res.send(result)
+    })
+
+    app.patch('/user/admin/:id', async (req, res) =>{
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)}
+      const updatedDoc = {
+        $set: {
+          role: 'admin'
+        }
+      }
+      const result = await userCollection.updateOne(filter, updatedDoc)
+      res.send(result)
+
+    })
+
+    // all products
+    app.get('/products', async(req, res) =>{
+        const result = await productsCollection.find().toArray();
+        res.send(result)
+    })
+
+    // cart store
+    app.post('/cart', async (req, res) =>{
+      const cartItem = req.body;
+      const result = await cartCollection.insertOne(cartItem);
+      res.send(result);
+    })
+
+    // load user based data 
+    app.get('/cart', async(req, res) =>{
+      const email = req.query.email;
+      const query = {email: email}
+      result = await cartCollection.find(query).toArray()
+      res.send(result)
+    })
+
+    // User can delte item
+    app.delete('/cart/:id', async(req, res) =>{
+      const id = req.params.id
+      const query = {_id: new ObjectId(id)}
+      const result = await cartCollection.deleteOne(query);
+      res.send(result)
+    })
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+// -------------
+
+app.get('/', (req, res) =>{
+    res.send('Fresh Food Deliverring')
+})
+
+app.listen(port, () =>{
+    console.log(`Fresh Food is Running ${port}`)
+});
